aa4310398841a9ad0a53a78f7f65f401eb0295fd
Marcos Guibson <mgss.lic17@uea.edu.br>
2020-03-07 12:57:14 -0400

    first commit

---
+++ estudosInterface2/estudosInterface2/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace estudosInterface2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var image = new Image { Source = "vai.png" };
+
+
+        }
+        void Impressao(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Iniciar impressão de Texto";
+        }
+        
+
+    }
+}
